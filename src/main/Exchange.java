9492a1e8d6b7746557462dedd7679c14245b98c9
Jurandi <jur.and.jr42@gmail.com>
2022-07-27 08:08:44 -0300

    Definiçaõ do método addRate na classe Exchange

--- src/main/Exchange.java
+++ src/main/Exchange.java
@@ -1,6 +1,10 @@
 package main;
 
+import java.util.Hashtable;
+
 public class Exchange {
+	
+	private Hashtable rates = new Hashtable();
 	
 	public Currency reduce(Expression source, String to) {
 		return source.reduce(this, to);
@@ -12,8 +16,8 @@ public class Exchange {
 				:1;
 	}
 
-	public void addRate(String string, String string2, int i) {
-
+	public void addRate(String from, String to, int rate) {
+		rates.put(new Pair(from, to), new Integer(rate));
 	}
 
 }
