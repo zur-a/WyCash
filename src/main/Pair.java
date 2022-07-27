236c0d6d335914a5a857ffb68d08c438e27335c1
Jurandi <jur.and.jr42@gmail.com>
2022-07-27 08:05:37 -0300

    Adição dos métodos equals e hashCode à classe Pair

--- src/main/Pair.java
+++ src/main/Pair.java
@@ -8,4 +8,17 @@ public class Pair {
 		this.from = from;
 		this.to = to;
 	}
+	
+	public boolean equals(Object object) {
+		Pair pair = (Pair) object;
+		return from.equals(pair.from)&&to.equals(pair.to);
+	}
+	
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((from == null) ? 0 : from.hashCode());
+		result = prime * result + ((to == null) ? 0 : to.hashCode());
+		return result;
+	}
 }
